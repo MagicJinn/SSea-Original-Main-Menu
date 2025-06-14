1f69d5f88f43ba736f045c5b27522cf2284994ef
MagicJinn <jinn.bz@mail.com>
2025-06-15 01:56:32 +0200

    Initial commit

--- Plugin.cs
+++ Plugin.cs
@@ -1,13 +1,11 @@
 ﻿using BepInEx;
-using BepInEx.Logging;
+using Sunless.Game.Scripts.Menus;
 
 namespace Original_Main_Menu;
 
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 public class Plugin : BaseUnityPlugin
 {
-    internal static new ManualLogSource Logger;
-
     private void Awake()
     {
         // Plugin startup logic
