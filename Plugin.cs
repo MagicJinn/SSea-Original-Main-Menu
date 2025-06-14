426b00db473b277a9b7864891930ab4abd344385
MagicJinn <jinn.bz@mail.com>
2025-06-15 01:38:16 +0200

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,16 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+
+namespace Original_Main_Menu;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+
+    private void Awake()
+    {
+        // Plugin startup logic
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+}
---
+++ obj/Debug/net35/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace Original_Main_Menu
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "Original_Main_Menu";
+        public const string PLUGIN_NAME = "My first plugin";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
